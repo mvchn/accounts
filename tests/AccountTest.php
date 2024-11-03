add4ec1a5fea921080f377419bead834d3752e6b
Red Rubber Duck <122934358+roman-duck@users.noreply.github.com>
2024-11-03 20:20:53 +0200

    Account, close #4

---
+++ tests/AccountTest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Tests;
+
+use App\Account;
+use PHPUnit\Framework\TestCase;
+
+class AccountTest extends TestCase
+{
+    public function testGetBalance(): void
+    {
+        $account = new Account();
+        $account->deposit(100);
+        $this->assertEquals(100, $account->getBalance());
+    }
+
+    public function testDeposit(): void
+    {
+        $account = new Account();
+        $account->credit(100);
+        $this->assertEquals(-100, $account->getBalance());
+    }
+
+    public function testWithdraw(): void
+    {
+        $account = new Account();
+        $account->deposit(100);
+        $account->withdraw(50);
+        $this->assertEquals(50, $account->getBalance());
+    }
+
+    public function testWithdrawException(): void
+    {
+        $account = new Account();
+        $account->deposit(100);
+        $this->expectException(\Exception::class);
+        $account->withdraw(150);
+    }
+
+    public function testCredit(): void
+    {
+        $account = new Account();
+        $account->deposit(100);
+        $account->credit(150);
+        $this->assertEquals(-50, $account->getBalance());
+    }
+}
