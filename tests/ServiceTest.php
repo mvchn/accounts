36e1020054ad33297877c0505ffa745935a266cb
Red Rubber Duck <mvch@triangle.software>
2024-11-08 17:07:57 +0200

    test fixes

---
+++ tests/ServiceTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+use App\Service;
+use App\Order;
+
+class ServiceTest extends TestCase
+{
+    public function testOrder(): void
+    {
+        $order = new Order('test', 'test', new \App\Money(100, 'USD'));
+        $order->setCapacity(10);
+        $this->assertEquals(10, $order->getCapacity());
+        $this->assertFalse($order->isActive());
+        $this->assertFalse($order->isActive());
+    }
+
+    public function testOrderException(): void
+    {
+        $order = new Order('test', 'test', new \App\Money(100, 'USD'));
+        $this->expectException(\Exception::class);
+        $order->send();
+    }
+
+    public function testOrderRequest(): void
+    {
+        $service = new Service('test', 'test');
+        $order = $service->orderRequest(['amount' => 1, 'currency' => 'USD', 'capacity' => 1, 'user_name' => 'test', 'service_name' => 'test']);
+        $this->assertEquals('test', $order->getUserName());
+        $this->assertEquals('test', $order->getServiceName());
+    }
+}
